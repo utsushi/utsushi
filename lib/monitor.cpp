bfb7df954f2237cc36a87f9e694fad8d4fff8703
Olaf Meeuwissen <paddy-hack@member.fsf.org>
2015-04-21 22:08:44 +0900

    Purge HAL support from the code base
    HAL has been deprecated from the major distributions since 2011 and
    the likes of Debian `squeeze`, Ubuntu `lucid` and Fedora 16 stopped
    including it in their default installs.
    
    See also: http://en.wikipedia.org/wiki/HAL_%28software%29#Deprecated

--- lib/monitor.cpp
+++ lib/monitor.cpp
@@ -29,9 +29,6 @@ extern "C" {                    // needed until libudev-150
 }
 #include <list>
 #endif
-#if HAVE_LIBHAL
-#include <libhal.h>
-#endif
 
 #include <cstdlib>
 
@@ -237,8 +234,6 @@ monitor::read (std::istream& istr)
 static void
 add_sane_udev (std::set<scanner::info>& devices, const char *key,
                const char *val);
-static void
-add_sane_hal  (std::set<scanner::info>& devices, const char *cap);
 
 static void
 add_conf_file  (std::set<scanner::info>& devices);
@@ -255,7 +250,6 @@ monitor::impl::impl ()
   //  that when creating a scanner::info object.
 
   add_sane_udev (devices_, "libsane_matched", "yes");
-  add_sane_hal  (devices_, "scanner");
 }
 
 #if (!HAVE_LIBUDEV)
@@ -422,136 +416,6 @@ add_sane_udev (std::set<scanner::info>& devices, const char *key,
 }
 
 #endif  /* HAVE_LIBUDEV */
-
-
-#if (!HAVE_LIBHAL)
-
-static void
-add_sane_hal (std::set<scanner::info>& devices, const char *cap)
-{}
-
-#else  /* HAVE_LIBHAL */
-
-//!  Picks up on SANE tagged scanner devices.
-/*!  This function filters on the device capability \a cap, as
-     normally specified in the SANE project's FDI file for use
-     with HAL.
-
-     \note  HAL is deprecated upstream in favour of udev.
-
-     \todo  Fix interface detection heuristics.
-     \todo  Add support for non-USB subsystem devices.
-     \todo  Beef up error checking/handling.
- */
-static void
-add_sane_hal (std::set<scanner::info>& devices, const char *cap)
-{
-  log::alert ("HAL based scanner detection has been disabled.\n"
-              "Contact the developers if support is needed.");
-
-  // This code is supposed to have worked at some point in the past
-  // when libudev didn't exist yet.  This is believed to have been at
-  // the time when libsane merging a 'scanner' capability resulted in
-  // that capability being set on the *device*.  With Ubuntu 10.04 and
-  // Debian wheezy this is no longer the case.  The capability is set
-  // on all of the device's *interfaces* breaking the detection logic.
-  // Doing so results in a very interesting picture of multi-function
-  // peripherals!
-  //
-  // With HAL being deprecated, there is little point in getting this
-  // fixed unless really needed by someone somewhere where libudev is
-  // not an option.
-
-  return;
-
-  using std::string;
-
-  LibHalContext *ctx = libhal_ctx_new ();
-  if (!ctx)
-    {
-      log::error ("libhal_ctx_new");
-      return;
-    }
-
-  DBusError err;
-  dbus_error_init (&err);
-  DBusConnection *cnx = dbus_bus_get (DBUS_BUS_SYSTEM, &err);
-
-  if (libhal_ctx_set_dbus_connection (ctx, cnx)
-      && libhal_ctx_init (ctx, &err))
-    {
-      char **udi = NULL;
-      int    cnt = 0;
-
-      udi = libhal_find_device_by_capability (ctx, cap, &cnt, &err);
-
-      if (!dbus_error_is_set (&err))
-        {
-          for (int i = 0; i < cnt; ++i)
-            {
-              char *sub = libhal_device_get_property_string
-                (ctx, udi[i], "linux.subsystem", &err);
-
-              if (!sub)
-                {
-                  if (dbus_error_is_set (&err))
-                    {
-                      log::error ("%1%: %2%") % err.name % err.message;
-                    }
-                  continue;
-                }
-
-              if (string ("usb") == sub)
-                {
-                  char **iface_udi = NULL;
-                  int    iface_cnt = 0;
-
-                  iface_udi = libhal_manager_find_device_string_match
-                    (ctx, "info.parent", udi[i], &iface_cnt, &err);
-
-                  for (int j = 0; j < iface_cnt; ++j)
-                    {
-                      //  This is based on the fact that all EPSON
-                      //  devices encountered so far always use the
-                      //  vendor specific class and there not being
-                      //  much of a viable alternative in the specs.
-
-                      if (255 == libhal_device_get_property_int
-                          (ctx, iface_udi[j], "usb.interface.class", &err))
-                        {
-                          char *mdl = libhal_device_get_property_string
-                            (ctx, iface_udi[j], "info.product", &err);
-                          char *vnd = libhal_device_get_property_string
-                            (ctx, iface_udi[j], "info.vendor", &err);
-                          char *path = libhal_device_get_property_string
-                            (ctx, iface_udi[j], "linux.sysfs_path", &err);
-
-                          std::string cnx ("usb");
-                          scanner::info info (cnx + "::" + path);
-                          if (mdl) info.model (mdl);
-                          if (vnd) info.vendor (vnd);
-
-                          devices.insert (info);
-                        }
-                    }
-                  libhal_free_string_array (iface_udi);
-                }
-            }
-        }
-      libhal_free_string_array (udi);
-      libhal_ctx_shutdown (ctx, NULL);
-    }
-
-  if (dbus_error_is_set (&err))
-    {
-      log::error ("%1%: %2%") % err.name % err.message;
-    }
-
-  LIBHAL_FREE_DBUS_ERROR (&err);
-  libhal_ctx_free (ctx);
-}
-
-#endif  /* HAVE_LIBHAL */
 
 
 //! Picks up scanner devices from configuration files
